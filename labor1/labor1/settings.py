9beae8d6a62e6c140d9728f9240c192f6e0333fc
Bertók Dániel <bertokd57@gmail.com>
2026-03-22 11:01:01 +0100

    configure new postgres db

--- labor1/labor1/settings.py
+++ labor1/labor1/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'labor1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(DATA_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'photogallery', 
+        'USER': 'dbuser',    
+        'PASSWORD': 'dbpass',  
+        'HOST': 'postgresql',
+        'PORT': '5432',
     }
 }
 
